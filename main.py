3f702b0f6676026ae98477fc5c0c0c07ec719b7a
Hpmanen-dev <79310444+Hpmanen-dev@users.noreply.github.com>
2021-02-19 19:06:44 +0100

    Added requirements into requirements.txt

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 """
 Welcome to Pig Game!
 
+How the game works:
+
 Each turn, a player repeatedly rolls a die until the player decides to hold or rolls a 1.
 If the player decides to hold he/she will add the total sum of all the dice he/she has rolled during that turn to his/her total points.
 If the dice shows a 1 the player will not receive any points and his/her turn will end letting the other player roll.
-The one to get to 100 points first is the winner.
+The one to get to 100 points or more first is the winner.
 """
 import commands
 
