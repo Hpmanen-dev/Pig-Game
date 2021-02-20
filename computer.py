d0b7bf95696bfbf23930fe1e41ee9dfce26172a7
Hpmanen-dev <79310444+Hpmanen-dev@users.noreply.github.com>
2021-02-20 19:25:59 +0100

    Added Docstring

--- computer.py
+++ computer.py
@@ -8,13 +8,17 @@ class Computer():
     Name = "Computer Player"
 
     def __init__(self):
+        """Initiate Computer."""
         self.Score = 0
 
     def get_Score(self):
+        """Get Score."""
         return self.Score
     
     def get_name(self):
+        """Get Computer Name."""
         return self.Name
     
     def add_Score(self, Score):
+        """Add to Computer's score."""
         self.Score += Score
