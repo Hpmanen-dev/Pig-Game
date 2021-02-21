860cbab7f74a04dae8d5276e8716d94bd4bd156d
Hpmanen-dev <79310444+Hpmanen-dev@users.noreply.github.com>
2021-02-21 20:37:47 +0100

    Added the makefile

--- dice.py
+++ dice.py
@@ -3,14 +3,14 @@ import random
 
 
 class Dice():
-    """A dice with 6 sides."""
+    """Create a six-sided dice."""
 
-    faces = 6
+    sides = 6
 
     def __init__(self):
         """Initiate a dice."""
         random.seed()
 
     def roll(self):
-        """Roll a dice once and return the value."""
-        return random.randint(1, self.faces)
+        """Roll the dice and return the number."""
+        return random.randint(1, self.sides)
