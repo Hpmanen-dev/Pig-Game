e9b43a31094114297dace3dfab13369894420ab3
Kristian <kristian381@hotmail.com>
2021-02-23 18:51:42 +0100

    Added a docsting to commands.py

--- commands.py
+++ commands.py
@@ -111,5 +111,5 @@ class Commands(cmd.Cmd):
             self.computer.set_intelligence(int_arg)
 
     def do_highscore(self, _):
-        """Display highscore"""
+        """Display highscore."""
         highscore.showHighscore()
